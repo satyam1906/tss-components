1f54f9b187610058cc974e1329c2e668dd2e15bd
satyam1906 <satyam9322@gmail.com>
2024-04-09 09:50:55 +0530

    Resolved Merge Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,17 @@ import ProductPage from "./components/ProductPage";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import CarouselCard from "./components/CarouselCard";
-<<<<<<< HEAD
 import Home from "./components/pages/Home";
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 1c6d0a3f6bfd97370a61d34ec681d5f0d91af9a8
 
 function App() {
   return (
     <>
       <BrowserRouter>
-<<<<<<< HEAD
       {(window.location.pathname !== '/signin' && window.location.pathname !== '/signup') && <AppBar />}
         <Routes>
           {/* <AppBar /> */}
           <Route path="/" element={<Home/>} />
-=======
-      {window.location.pathname !== '/signin' && <AppBar />}
-          {/* <AppBar /> */}
-        <Routes>
->>>>>>> 1c6d0a3f6bfd97370a61d34ec681d5f0d91af9a8
           <Route path="/product" element={<ProductCardContainer />} />
           <Route path="/product-page" element={<ProductPage />} />
           <Route path="/signin" element={<SignIn />} />
